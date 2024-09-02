18ce0d54971ea8d34e92e4c633eed58a49028854
Cristian Melgarejo <cristancho1994@hotmail.com>
2024-09-01 21:25:35 -0500

    Reto ADL - Operadores RxJs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
-const inputQuery = document.createElement('input');
-document.querySelector('body').append(inputQuery);
+import { fromEvent, Observer, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { catchError, debounceTime, map, switchMap } from "rxjs/operators";
+
+const inputQuery = document.getElementById('character-name');
+
+const inputEvent$ = fromEvent<KeyboardEvent>(inputQuery, 'keyup').pipe(
+    debounceTime(1000),
+    map((event) =>{
+      const input = event.target as HTMLInputElement;
+      return input.value;
+    }),
+    switchMap((character)=>{
+      return ajax.getJSON(`https://rickandmortyapi.com/api/character/?name=${character}`)
+    }),
+    map((response:any) => {
+      const firstCharacter = response?.results[0];
+      return {
+        name: firstCharacter.name,
+        image: firstCharacter.image,
+        gender: firstCharacter.gender,
+        species: firstCharacter.species,
+      };
+    }),
+    catchError((err:any) => {
+      console.error(err);
+      return of({
+        name: "Alien Googah",
+        image: "https://rickandmortyapi.com/api/character/avatar/14.jpeg",
+        gender: "unknown",
+        species: "Alien"
+      });
+    })
+);
+
+inputEvent$.subscribe((chareacterFound) => {
+  console.log(chareacterFound);
+  const nameCharacterDiv = document.getElementById('name-character');
+  nameCharacterDiv.innerText = `Name: ${chareacterFound.name}`;
+  const genderCharacterDiv = document.getElementById('gender-character');
+  genderCharacterDiv.innerText = `Gender: ${chareacterFound.gender}`;
+  const speciesCharacterDiv = document.getElementById('species-character');
+  speciesCharacterDiv.innerText = `Specie: ${chareacterFound.species}`;
+  const imageCharacterDiv = document.getElementById('image-character') as HTMLImageElement;
+  imageCharacterDiv.src = chareacterFound.image;
+});
